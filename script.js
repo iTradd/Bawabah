ee07db89952d67315e1936da0dc30bf48f95a94b
iTradd <126025494+iTradd@users.noreply.github.com>
2024-12-13 01:15:55 +0300

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-const apiUrl = 'https://script.google.com/macros/s/AKfycbxpxdz6wqCgTBB5gKMOkcKUfUAyYejiojRmgO777f2pD_xcK4a6bSxu5qn0vAQiYsJD/exec';
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxpxdz6wqCgTBB5gKMOkcKUfUAyYejiojRmgO777f2pD_xcK4a6bSxu5qn0vAQiYsJD/exec';
+const apiUrl = 'https://script.google.com/macros/s/AKfycbxJ_dBo6dxk14hDkDi26eQjKI6MNjuxNqjYmMPXqS65JOAoUQLzkP8tWiq67lXZPySy/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxJ_dBo6dxk14hDkDi26eQjKI6MNjuxNqjYmMPXqS65JOAoUQLzkP8tWiq67lXZPySy/exec';
+
+
