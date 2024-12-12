aa3339b98adcb4adf57e073f72f8b7c37d4c3d51
iTradd <126025494+iTradd@users.noreply.github.com>
2024-12-12 18:31:11 +0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// B1'!) BJE) API_URL EF E*:J1 'D(J&)
+// قراءة قيمة API_URL من متغير البيئة
 const apiUrl = process.env.API_URL;
 
-// '3*./'E 'DE*:J1
+// استخدام المتغير
 fetch(`${apiUrl}?action=getProperties&sheet=Real_Estate`)
     .then(response => response.json())
-    .then(data => console.log(data)) // JECFC '3*(/'D console.log (9EDJ) #.1I DE9'D,) 'D(J'F'*
+    .then(data => console.log(data)) // يمكنك استبدال console.log بعملية أخرى لمعالجة البيانات
     .catch(error => console.error('Error fetching properties:', error));
