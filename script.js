394a47fdca0f029fde2bb5740fdecbc8c3b0affa
iTradd <126025494+iTradd@users.noreply.github.com>
2024-12-20 05:12:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // الرابط الجديد الخاص بـ Cloudflare Worker
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwq2lOi4KBk1ef_-7bhDMzabEKsnF7qdtHtdejyrh-oUwZrimpZsZvtjkLPGiTrikk6/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzRbpRHlHAeWUQChaJ9SToZ2_V7FZh4EJOWiAnfjTxoMhGx7Jhk2lYFrjFhHbAxNCs/exec';
 
 //Javascript for Property!!
 
