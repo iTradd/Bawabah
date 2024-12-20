1848a8a1050ff81710a938d9fa980999fd916a58
iTradd <126025494+iTradd@users.noreply.github.com>
2024-12-20 03:35:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 // الرابط الجديد الخاص بـ Cloudflare Worker
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwCxAHB6QIKMYIhlCqo55m4Eb0pXaVuwa1a21XN_O1bZtm1aVRTaDv4uBx6390qksBV/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwq2lOi4KBk1ef_-7bhDMzabEKsnF7qdtHtdejyrh-oUwZrimpZsZvtjkLPGiTrikk6/exec';
